6cab65144e7bd97f7f528ea57f4933bb1aaac268
Yannick Felix <yannick.felix1999@gmail.com>
2016-03-06 21:20:18 +0100

    LogsEntry Method Stubs Permissions Stub

--- classes/LogEntry.php
+++ classes/LogEntry.php
@@ -10,5 +10,57 @@ namespace Entrance;
 
 
 class LogEntry {
+    private $lID, $uID, $timestamp, $action, $success;
 
+    /**
+     * LogEntry constructor.
+     * @param $uID
+     * @param $timestamp
+     * @param $action
+     * @param $success
+     */
+    public function __construct($uID, $timestamp, $action, $success) {
+        $this->uID = $uID;
+        $this->timestamp = $timestamp;
+        $this->action = $action;
+        $this->success = $success;
+    }
+
+    public static function getAllLogs($limit = 0) {
+
+    }
+
+    public static function fromLID($lID) {
+
+    }
+
+
+
+    /**
+     * @return mixed
+     */
+    public function getUID() {
+        return $this->uID;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTimestamp() {
+        return $this->timestamp;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAction() {
+        return $this->action;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSuccess() {
+        return $this->success;
+    }
 }
---
+++ classes/Permissions.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yanni
+ * Date: 06.03.2016
+ * Time: 21:19
+ */
+
+namespace Entrance;
+
+define("PERM_DB_LOGIN", "admin.database");
+
+define("PERM_USER_DELETE", "users.del");
+define("PERM_USER_EDIT",   "users.edit");
+define("PERM_USER_CREATE", "users.create");
+define("PERM_USER_VIEW", "users.view");
+define("PERM_USER_EDIT_PERMISSIONS", "users.perms");
